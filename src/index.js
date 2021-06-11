bab8586b9f55d2816f28e0110635bb1fb68bf693
devWRM <mcwen@comcast.net>
2021-06-10 21:28:35 -0400

    imported redux, react-redux, and middleware packages to set up the Redux store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+import { rootReducer } from './reducers/rootReducer.js';
 import './index.css';
 import App from './App';
 
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+)
+
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
