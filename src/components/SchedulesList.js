8436b135ded5282ae9497610d2bb5494b5bce745
devWRM <mcwen@comcast.net>
2021-06-15 14:52:56 -0400

    mapped user's to show each of their schedules

--- src/components/SchedulesList.js
+++ src/components/SchedulesList.js
@@ -1,11 +1,53 @@
 import React from 'react'
 
-function SchedulesList() {
+import { connect } from 'react-redux';
+
+function SchedulesList(props) {
+
+// debugger
+//  props.users[0].schedules[0].dated
+
     return (
         <div>
+
             SchedulesList
+            {/* { props.users.length ? console.log(props.users[0].schedules[0].active) : console.log("HELLO!!!") } */}
+
+            { props.users.length ? 
+                 props.users.map(user => 
+                    <p key={user.id}>
+
+                        {/* { user.schedules[0].active ? "active" : "NOT active" } */}
+
+                         
+                            { user.schedules.length ? 
+                           
+                                user.schedules.map(schedule =>
+                                    <p>{user.name} "schedule"</p>
+                                )
+
+                                : "!NOT!"
+                            }
+                        
+                        
+
+                    </p>
+                )
+                // console.log(props.users[0].schedules[0].active) 
+            
+            
+            
+                : "HELLO!!!" 
+            }
+
+
         </div>
     )
 }
 
-export default SchedulesList
+const mapStateToProps = (state) => {
+    
+    return  { users: state.users }
+}
+
+export default connect(mapStateToProps)(SchedulesList);
