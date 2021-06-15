dff6b05d6f72625a2ced70ec130418005b54b408
devWRM <mcwen@comcast.net>
2021-06-14 20:08:55 -0400

    created SchedulesContainer class component

---
+++ src/components/SchedulesContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SchedulesContainer extends Component {
+    render() {
+        return (
+            <div>
+                YES!! THIS Schedules container
+            </div>
+        )
+    }
+}
+
+export default SchedulesContainer;
