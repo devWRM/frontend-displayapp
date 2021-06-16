6fc74cf13d5adc2b1e92700e546e9d03c7c726e2
devWRM <mcwen@comcast.net>
2021-06-16 09:38:54 -0400

    created ActiveUsersList functional component & added identifying header

---
+++ src/components/ActiveUsersList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ActiveUsersList() {
+    return (
+        <div>
+            <b>ONLY ACTIVE USERS (ActiveUsersList):</b>
+
+            
+
+        </div>
+    )
+}
+
+export default ActiveUsersList
