585b6a9eb51bdcbca6669aedbfd56ecc182659f5
devWRM <mcwen@comcast.net>
2021-06-16 09:39:58 -0400

    imported & displayed ActiveUsersList

--- src/components/ActiveUsersContainer.js
+++ src/components/ActiveUsersContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
+import ActiveUsersList from './ActiveUsersList'
+
 class ActiveUsersContainer extends Component {
     render() {
         return (
             <div>
-                
+                <ActiveUsersList />
             </div>
         )
     }
