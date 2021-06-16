e6f747b1b3281cc2c678e45bc54925293727f31d
devWRM <mcwen@comcast.net>
2021-06-16 09:22:42 -0400

    created SchedDetailsContainer class component

---
+++ src/components/SchedDetailsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class SchedDetailsContainer extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default SchedDetailsContainer
