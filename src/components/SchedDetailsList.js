bf9722d04579899881d50d64cfa39c4e01fb0ab3
devWRM <mcwen@comcast.net>
2021-06-16 20:50:26 -0400

    created component identifying header inside SchedDetailsList functional component

--- src/components/SchedDetailsList.js
+++ src/components/SchedDetailsList.js
@@ -3,9 +3,9 @@ import React from 'react'
 function SchedDetailsList() {
     return (
         <div>
-            
+            <b>ALL details for User,Room,Cleaning (SchedDetailsList):</b>
         </div>
     )
 }
 
-export default SchedDetailsList
+export default SchedDetailsList;
