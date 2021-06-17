ef9273996cd622d9b673e5de63df0cd72ed1bd67
devWRM <mcwen@comcast.net>
2021-06-16 20:49:11 -0400

    imported & returned SchedDetailsList

--- src/components/SchedDetailsContainer.js
+++ src/components/SchedDetailsContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 
+import SchedDetailsList from './SchedDetailsList.js';
+
 class SchedDetailsContainer extends Component {
     render() {
         return (
             <div>
-                
+                <SchedDetailsList />
             </div>
         )
     }
