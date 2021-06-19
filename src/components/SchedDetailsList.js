8f359d597641f99cff8b1c1c391778c06fd82342
devWRM <mcwen@comcast.net>
2021-06-19 19:02:45 -0400

    added and invoked formattedDate function & imported Rooms & passed user and schedule props to Rooms component to find correct room for display

--- src/components/SchedDetailsList.js
+++ src/components/SchedDetailsList.js
@@ -1,11 +1,62 @@
 import React from 'react'
 
-function SchedDetailsList() {
+import { connect } from 'react-redux';
+import Rooms from './Rooms.js';
+
+function SchedDetailsList(props) {
+
+// debugger;
+
+    function formattedDate(date) {
+        let sliceDate = date.slice(0,10)
+        let splitDate = sliceDate.split("-")
+        let year = splitDate[0];
+        let month = splitDate[1];
+        let day = splitDate[2];
+      
+        return month + '/' + day + '/' + year;
+    }
+
+
     return (
         <div>
             <b>ALL details for User,Room,Cleaning (SchedDetailsList):</b>
+            <br />
+
+            {props.users.length ?               
+                props.users.map( user => 
+                    <span key={user.id}> 
+
+                        
+                        {/* { user.schedules.length ? user.name : null }  */}
+                        <b>{user.name}</b>
+
+                        {user.schedules.length ? 
+                            
+                            user.schedules.map( schedule =>
+                                <li key={schedule.id}>
+                                    {formattedDate(schedule.dated)} 
+                                    <Rooms user={user} schedule={schedule} />
+                                </li>
+                                
+                            )
+                            
+                            : <li>has no schedules</li>
+                        }
+
+                    </span>
+
+                )
+                
+        
+                : "There are no users."
+            }
         </div>
     )
 }
 
-export default SchedDetailsList;
+const mapStateToProps = (state) => {
+    return { users: state.users }
+}
+
+export default connect(mapStateToProps)(SchedDetailsList);
